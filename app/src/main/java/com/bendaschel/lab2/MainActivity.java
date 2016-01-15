240e28e9303da1e5b266f9412a69672865619b05
Benjamin Daschel <coffeemaxed@gmail.com>
2016-01-15 12:40:24 -0800

    working drawer and image views

--- app/src/main/java/com/bendaschel/lab2/MainActivity.java
+++ app/src/main/java/com/bendaschel/lab2/MainActivity.java
@@ -3,45 +3,84 @@ package com.bendaschel.lab2;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Bind(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
 
     @Bind(R.id.lv_drawer)
-    RelativeLayout mDrawerListView;
+    ListView mDrawerListView;
+
+    @Bind(R.id.imageView)
+    ImageView mImageView;
 
     private ActionBarDrawerToggle mActionBarToggle;
+    private CharSequence mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        mDrawerLayout.setDrawerTitle(GravityCompat.END, "Navigation drawer");
+        mActivityTitle = getTitle();
         mActionBarToggle = new ActionBarDrawerToggle(
                 this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        ){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                setActionBarTitle(getString(R.string.navigation_drawer_open));
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                setActionBarTitle(mActivityTitle);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mActionBarToggle);
+        mDrawerListView.setOnItemClickListener(this);
+        setupListView();
+    }
+
+    private void setupListView(){
+        mDrawerListView.setAdapter(
+                new ArrayAdapter<ListItem>(this, R.layout.item_nav_drawer, ListItem.values())
         );
     }
 
-//    @OnItemClick(R.id.lv_drawer)
-//    @Override
-//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//        mDrawerLayout.closeDrawers();
-//
-//    }
+    private void setActionBarTitle(CharSequence title){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setTitle(title);
+        }
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("MainActivity", "Item Clicked");
+        ListItem item = ListItem.values()[position];
+        mImageView.setImageResource(item.getId());
+        mDrawerLayout.closeDrawer(mDrawerListView);
+    }
 }
--- app/src/test/java/com/bendaschel/lab2/MainActivityTest.java
+++ app/src/test/java/com/bendaschel/lab2/MainActivityTest.java
@@ -1,15 +1,33 @@
 package com.bendaschel.lab2;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertNotSame;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class MainActivityTest {
 
+    @Test
+    public void testDrawerToggle(){
+
+        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+        DrawerLayout drawer = (DrawerLayout) mainActivity.findViewById(R.id.drawer_layout);
+        CharSequence initialTitle = mainActivity.getSupportActionBar().getTitle();
+        drawer.openDrawer(GravityCompat.START);
+        CharSequence newTitle = mainActivity.getSupportActionBar().getTitle();
+        assertThat(initialTitle, not(newTitle));
+    }
 }
