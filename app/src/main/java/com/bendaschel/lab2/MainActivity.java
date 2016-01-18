8555b9c74c772632bb2ba6cb44a49138aec1e82d
Benjamin Daschel <coffeemaxed@gmail.com>
2016-01-18 09:22:25 -0800

    clean up imports

--- app/src/main/java/com/bendaschel/lab2/MainActivity.java
+++ app/src/main/java/com/bendaschel/lab2/MainActivity.java
@@ -1,14 +1,11 @@
 package com.bendaschel.lab2;
 
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,14 +13,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
